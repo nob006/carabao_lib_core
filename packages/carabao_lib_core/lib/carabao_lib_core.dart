8032a568d347454d29127b6aea720486db577757
nob <nop.gpk@gmail.com>
2020-09-15 10:34:00 +0700

    just mock some method for new packets

--- packages/carabao_lib_core/lib/carabao_lib_core.dart
+++ packages/carabao_lib_core/lib/carabao_lib_core.dart
@@ -4,4 +4,8 @@ library carabao_lib_core;
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+
+  String getStringFromCore(){
+    return "Text From Core Module";
+  }
 }
